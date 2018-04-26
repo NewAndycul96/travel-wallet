331b44a972d1029764cc43fd34d7bb272ef3d918
Anand Kulkarni <andycul96@yahoo.com>
2018-04-26 18:56:23 -0500

    sixth Commit

--- Travel Wallet/DateViewController.swift
+++ Travel Wallet/DateViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class DateViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    var startDate = Date()
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,16 +25,41 @@ class DateViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+//    func datePickerValueChanged (datePicker: UIDatePicker) {
+    
+//        let dateformatter = DateFormatter()
+//
+//        dateformatter.dateStyle = DateFormatter.Style.medium
+//
+//       // let dateValue = dateformatter.string(from: datePicker.date)
+//
+//        startDate = datePicker.date
+//        print(startDate)
+//    }
+//
+//
+    
+//    func pickerView(datePicker: UIDatePicker, didSelectRow row: Int, inComponent component: Int) {
+//        print(datePicker.date)
+//    }
+    
+    @IBAction func valueDidChange(_ sender: UIDatePicker) {
+        print(sender.date)
+    }
     
 
-    /*
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//
+//        let destination = segue.destination as! AddtripViewController
+//        destination.startDate =
+    
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
-    */
+    //}
 
 }
