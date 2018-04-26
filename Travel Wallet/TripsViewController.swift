2296156eedc9cdc32fd89e94cf4b6d6672a833df
Anand Kulkarni <anandkulkarni@mwc-067129.dhcp.missouri.edu>
2018-04-26 12:52:11 -0500

    First commit

---
+++ Travel Wallet/TripsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Travel Wallet
+//
+//  Created by Anand Kulkarni on 4/3/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
